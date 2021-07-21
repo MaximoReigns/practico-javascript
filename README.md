9004809cfa2c19b6878a4ed1cdf92d2e90119006
Máximo Sánchez <id569723@outlook.com>
2021-07-21 16:07:42 -0500

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # practico-javascript
+
+...
+
+## Talle #1: Figuras geométricas
+
+- Primer paso: definir las fórmulas
+- Segundo: Implementar las fórmulas en JavaScript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JS con HTML
---
+++ figuras.js
@@ -0,0 +1,45 @@
+// Código del cuadrado
+console.group("Cuadrados")
+
+function perimetroCuadrado(lado){
+    return lado * 4;
+}
+
+function areaCuadrado(lado){
+    return lado * lado;
+}
+
+console.groupEnd();
+
+//Código del Triangulo
+
+function perimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base;
+} 
+
+function areaTriangulo(base, altura){
+    return (base * altura) / 2;
+} 
+
+console.groupEnd();
+
+//Código del Circulo
+console.group("Círculos");
+
+//Circunferencia
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro * Math.PI;
+}
+
+//Diametro
+function diametroCirculo(radio){
+    return radio * 2;
+}
+
+//Area
+function areaCirculo(radio){
+    return (radio * radio) * Math.PI;
+}
+
+console.groupEnd();
